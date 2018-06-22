5cfaf2f334075a073ba8173bceb65bfd691334e5
Jonathan Zarate <zaratedev@gmail.com>
2018-06-22 13:50:51 -0500

    Using the directive for by while

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -4,20 +4,13 @@ class PrimeFactors
 {
     public function generate($number) {
         $primes = [];
-        $candidate = 2;
 
-        /*if ($number == 4) {
-            $primes = [2, 2];
-        }*/
-
-        while ($number > 1) {
+        for($candidate = 2; $number > 1; $candidate++) {
             while ($number % $candidate == 0) {
                 $primes[] = $candidate;
 
                 $number /= $candidate;
             }
-
-            $candidate++;
         }
 
         return $primes;
