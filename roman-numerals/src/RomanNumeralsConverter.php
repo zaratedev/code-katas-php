182df2d70faf24880f362ed7654f17efe4953271
Jonathan Zarate <zaratedev@gmail.com>
2018-06-22 15:09:08 -0500

    Translate a number into its roman numberal. use phpesec for testing

---
+++ roman-numerals/src/RomanNumeralsConverter.php
@@ -0,0 +1,59 @@
+<?php
+
+class RomanNumeralsConverter {
+
+    protected static $lookup = [
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I'
+    ];
+
+    public function convert($number){
+
+        $solution = '';
+
+        foreach (static::$lookup as $limit => $glyph) {
+            while ($number >= $limit) {
+                $solution .= $glyph;
+                $number -= $limit;
+            }
+        }
+
+       /* while ($number >= 9) {
+            $solution .= 'IX';
+            $number -= 9;
+        }
+
+
+        while ($number >= 5) {
+            $solution .= 'V';
+            $number -= 5;
+        }
+
+        while ($number >= 4) {
+            $solution .= 'IV';
+            $number -= 4;
+        }
+
+        while ($number >= 1) {
+            $solution .= 'I';
+            $number -= 1;
+        }*/
+
+        //$solution .= str_repeat('I', $number);
+
+        //return str_repeat('I', $number);
+
+        return $solution;
+    }
+}
